cb1ac167a82e626ed18ec7228ece55dbe69c8f14
Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
2026-03-17 12:58:29 +0530

    misc: Replace mount-based web image script with mtools
    Rename cfi.sh to create_webimage.sh for clarity. Replace the
    sudo mount/cp/umount workflow with mcopy from mtools to copy
    web pages into the FAT image directly, eliminating the need for
    root privileges. Also remove the checked-in web.img binary.
    
    Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>

--- misc/create_webimage.sh
+++ misc/create_webimage.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 #******************************************************************************
 
-rm web.img
+rm -f web.img
 
 # Create image file of 96K
 dd if=/dev/zero of=web.img bs=512 count=192
@@ -11,13 +11,6 @@ dd if=/dev/zero of=web.img bs=512 count=192
 # Format image with FAT
 /sbin/mkfs.vfat -F 12 web.img
 
-# Mount it
-sudo mkdir /tmp/fs
-chmod 666 web.img
-sudo mount -t vfat -o loop,rw web.img /tmp/fs/
-
-# Copy webpages
-sudo cp -r  ./web_pages/* /tmp/fs/
-sudo umount /tmp/fs
-sudo rm -R /tmp/fs
+# Copy webpages using mtools (no mount/sudo needed)
+mcopy -i web.img -s ./web_pages/* ::
 chmod 444 web.img
